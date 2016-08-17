ab300f03eb9c104903911c58f07f695de9b5ad16
Szewek <shevkpl@gmail.com>
2016-08-17 18:42:27 +0200

    Check if Minecraft-Flux is loaded without specified version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.1.1
+- [Mod] Check if Minecraft-Flux is loaded without specified version.
+
 ## 0.1.0
 - First version
--- src/main/java/szewek/mcflux/MCFluxMod.java
+++ src/main/java/szewek/mcflux/MCFluxMod.java
@@ -1,8 +1,29 @@
 package szewek.mcflux;
 
-import net.minecraftforge.fml.common.Mod;
 
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Just to clarify that this mod is loaded.
+ */
 @Mod(modid = R.MCFLUX_NAME, version = R.MCFLUX_VERSION)
 public class MCFluxMod {
-	// Just to clarify that this mod is loaded.
+	private Logger log;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		log = e.getModLog();
+		if (R.MCFLUX_VERSION.charAt(0) == '$')
+			log.warn("You are running Minecraft-Flux with an unknown version");
+	}
+	
+	public void init(FMLInitializationEvent e) {
+	}
+	public void postInit(FMLPostInitializationEvent e) {
+	}
 }
