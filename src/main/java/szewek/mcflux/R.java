d2b66596f3a0a8e91e116729e7d5123164cbe8ab
Szewek <shevkpl@gmail.com>
2016-08-21 19:09:13 +0200

    Better version handling

--- src/main/java/szewek/mcflux/R.java
+++ src/main/java/szewek/mcflux/R.java
@@ -2,7 +2,7 @@ package szewek.mcflux;
 
 public final class R {
 	public static final String
-		MCFLUX_VERSION = "${version}",
+		MCFLUX_VERSION = "@VERSION@",
 		MCFLUX_NAME = "mcflux",
 		MCFLUX_API_NAME = "mcfluxAPI",
 		MCFLUX_FLAVORAPI_NAME = "mcfluxAPI|flavor";
